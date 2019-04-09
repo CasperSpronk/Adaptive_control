70507a149ebc33fb17bb93f47361cf609df6335a
CasperSpronk <33654486+CasperSpronk@users.noreply.github.com>
2019-04-09 21:08:38 +0200

    changed plots

--- SC42120_Final_Assignment_Code_second_part/SC42120_Optimal_switching_and_control.m
+++ SC42120_Final_Assignment_Code_second_part/SC42120_Optimal_switching_and_control.m
@@ -4,14 +4,14 @@
 %%% 
 %%% Reference paper: Optimal Switching and Control of Nonlinear Switching Systems Using Approximate Dynamic Programming 
 %%% Authors of the paper: Ali Heydari and S.N. Balakrishnan
-%%% Journal: Applied Soft Computing, Vol. 24, 2014, pp. 291�303
+%%% Journal: Applied Soft Computing, Vol. 24, 2014, pp. 291???303
 %%% 
 %%% Copyright 2013 by Ali Heydari (heydari.ali@gmail.com)
 %%% Author Webpage: http://webpages.sdsmt.edu/~aheydari/
 %%%
 %%% Refer to the paper and the notations used therein for understanding this code
 %%%
-%%% Author of the Code: 
+%%% Author of the Code:                          
 %%% Vittorio Giammarino
 %%% DSCS, Tu Delft, 2018
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -29,11 +29,11 @@ psi = @(S,xtf)(xtf'*S*xtf);             %DONE
 %% Systems Dynamics in continuous 
 
 % System 1 dynamics
-f_1 = @(x1,x2)[x2;x1^2-x^2];            %DONE
+f_1 = @(x1,x2)[x2;x1^2-x2^2];           %Done
 g_1 = [0;1];                            %DONE
 
 % System 2 dynamics
-f_2 = @(x2)[x2;-x2];                    %DONE
+f_2 = @(x1,x2)[x2;-x2];                 %DONE
 g_2 = [0;0.5];                          %DONE
 
 % Quadratic Lyapunov Design 
@@ -108,14 +108,16 @@ MaxEpochNo= 10; % Unlike Algorithm 1 of the paper, where "beta" was defined for 
 NoOfEquations = 500; % Number of sample "x" selected for training using least squares, in the referenced paper this value is called "n"
 StateSelectionWidth = 1.25; % the states will be selected from interval (-StateSelectionWidth, StateSelectionWidth)
 
-Q1_bar = dt*Q; % Discretized Q1                     %DONE
-Q2_bar = dt*Q; % Discretized Q2                     %DONE
-R1_bar = dt*R; % Discretized R1                     %DONE
-R2_bar = dt*R; % Discretized R2                     %DONE
-f_1_bar = @(x)(x+dt*f_1(x)) ; % Discretized f_1     %DONE
-f_2_bar = @(x)(x+dt*f_2(x)); % Discretized f_2      %DONE
-g_1_bar = @(x)(dt*g_1(x)); % Discretized g_1        %DONE
-g_2_bar = @(x)(dt*g_2(x)); % Discretized g_2        %DONE
+
+Q1_bar = t_switch*dt*Q;                              % Discretized Q1        %DONE                
+Q2_bar = t_switch*dt*Q;                              % Discretized Q2        %DONE
+R1_bar = t_switch*dt*R;                              % Discretized R1        %DONE
+R2_bar = t_switch*dt*R;                              % Discretized R2        %DONE
+f_1_bar = @(x1,x2)([x1;x2]+t_switch*dt*f_1(x1,x2)); % Discretized f_1       %DONE
+f_2_bar = @(x1,x2)([x1;x2]+t_switch*dt*f_2(x1,x2));  % Discretized f_2       %DONE
+g_1_bar = @(x1,x2)(t_switch*dt*g_1);                 % Discretized g_1       %DONE
+g_2_bar = @(x1,x2)(t_switch*dt*g_2);                 % Discretized g_2       %DONE
+
 
 tic
 W = zeros(length(phi(X_k(1), X_k(2), t_switch)),N,MaxEpochNo);
@@ -146,8 +148,7 @@ for t = 0:N-1 % time goes from 0 to tf ----> N number of sample
                 X_k =  (rand(n,1)*2-1)* StateSelectionWidth; %Initial states selection
                 t_switch = (tf*rand(s-1,1));
                 
-                J_k_t = %TODO
-                
+                J_k_t = psi(S,X_k); %DONE
                 % J_k_t is the target. So, W_N'*phi(X_k) is supposed to approximate J_k_t. Let's store them in the following matrices for least squares
                 RHS_J(j,:) = J_k_t;
                 LHS_J(j,:) = phi(X_k(1), X_k(2), t_switch)';
@@ -156,7 +157,7 @@ for t = 0:N-1 % time goes from 0 to tf ----> N number of sample
                 fprintf('det phi = 0\n');
                 break;
             end
-            FinalW(:,k) = %TODO
+            FinalW(:,k) = pinv(LHS_J' * LHS_J) * LHS_J' * RHS_J; %DONE
             
         else % Step 2, k=N-1 to k=0
             % Step 3, i=1 and select a guess on V_0
@@ -170,11 +171,11 @@ for t = 0:N-1 % time goes from 0 to tf ----> N number of sample
                     temp_U_k = Vi_1(:,k,i)'*sigma(X_k(1), X_k(2), t_switch);
                     % Step 6
                     if k*dt < t_bar 
-                        X_k_plus_1 = %TODO
-                        U_k = %TODO
+                        X_k_plus_1 = f_1_bar(X_k(1),X_k(2)) + g_1_bar(X_k(1),X_k(2))*temp_U_k; %DONE
+                        U_k = -(R1_bar)^-1 * (g_1_bar(X_k(1),X_k(2)))'*dphi_dx(X_k(1),X_k(2),t_switch)'*FinalW(:,k+1);%TODO
                     else
-                        X_k_plus_1 = %TODO
-                        U_k = %TODO
+                        X_k_plus_1 = f_2_bar(X_k(1),X_k(2)) + g_2_bar(X_k(1),X_k(2))*temp_U_k; %DONE
+                        U_k = -(R2_bar)^-1 * (g_2_bar(X_k(1),X_k(2)))'*dphi_dx(X_k(1),X_k(2),t_switch)'*FinalW(:,k+1);%TODO
                     end
                     RHS_U(j,:) = U_k';
                     LHS_U(j,:) = sigma(X_k(1), X_k(2), t_switch)';
@@ -184,7 +185,7 @@ for t = 0:N-1 % time goes from 0 to tf ----> N number of sample
                     break;
                 end
                 % Step 7
-                Vi_1(:,k,i+1) = %TODO
+                Vi_1(:,k,i+1) = pinv(LHS_U' * LHS_U) * LHS_U' * RHS_U;%DONE
                 
                 % Step 8
                 % Check to see if the weights of the actors in fixed point
@@ -205,13 +206,13 @@ for t = 0:N-1 % time goes from 0 to tf ----> N number of sample
                 t_switch = (tf*rand(s-1,1)); %Initial states selection
                 temp_U_k = FinalV(:,k)'*sigma(X_k(1), X_k(2), t_switch);
                 if k*dt < t_bar 
-                   X_k_plus_1 = %TODO
-                   J_k_plus_1 = %TODO
-                   J_k_t = %TODO
+                   X_k_plus_1 =  f_1_bar(X_k(1),X_k(2)) + g_1_bar(X_k(1),X_k(2)) * temp_U_k; %DONE
+                   J_k_plus_1 = FinalW(:,k+1)' * phi(X_k_plus_1(1),X_k_plus_1(2),t_switch); %DONE
+                   J_k_t = 0.5*Q1_bar(1,1)+0.5*sigma(X_k(1), X_k(2), t_switch)'*FinalV(:,k)*R1_bar*FinalV(:,k)'*sigma(X_k(1), X_k(2), t_switch)+J_k_plus_1;%TODO
                 else
-                   X_k_plus_1 = %TODO
-                   J_k_plus_1 = %TODO
-                   J_k_t = %TODO
+                   X_k_plus_1 =  f_2_bar(X_k(1),X_k(2)) + g_2_bar(X_k(1),X_k(2)) * temp_U_k;%DONE
+                   J_k_plus_1 = FinalW(:,k+1)' * phi(X_k_plus_1(1),X_k_plus_1(2),t_switch);%DONE
+                   J_k_t = 0.5*Q2_bar(2,2)+0.5*sigma(X_k(1), X_k(2), t_switch)'*FinalV(:,k)*R2_bar*FinalV(:,k)'*sigma(X_k(1), X_k(2), t_switch)+J_k_plus_1;%TODO
                 end
                 RHS_J(j,:) = J_k_t;
                 LHS_J(j,:) = phi(X_k(1), X_k(2), t_switch)';
@@ -220,7 +221,7 @@ for t = 0:N-1 % time goes from 0 to tf ----> N number of sample
                 fprintf('det phi = 0\n');
                 break;
             end
-            FinalW(:,k) = %TODO
+            FinalW(:,k) = pinv(LHS_J' * LHS_J) * LHS_J' * RHS_J; %DONE
             if isnan(FinalW(:,k))
                 fprintf('Training W is diverging...\n');
                 diverged = 1;
@@ -317,3 +318,86 @@ plot(samples, Conv_check(:,1))
 xlabel('samples')
 ylabel('convergence')
 
+%% plot cost function 
+
+%x0 = [-1 0.5]';
+x0 = [0 -1]';
+
+dt_switch = 0.001;
+
+t_switch = 0:dt_switch:tf;
+J0 = zeros(1,length(t_switch));
+
+for j=1:length(t_switch)
+J0(1,j) = FinalW(:,1)'*phi(x0(1),x0(2),t_switch(1,j));
+end
+
+[x,t] = min(J0);
+l = t*dt_switch;
+
+figure()
+plot(t_switch, J0)
+xlabel('t_{switch}')
+ylabel('J0')
+
+[M,I]=min(J0(1,1:end));
+t_1 = I*dt_switch;
+Xd_k = zeros(n,N);
+Ud_k = zeros(1,N);
+Xd_k(:,1) = x0;
+
+
+time = dt:dt:2;
+
+
+for k = 1:N-1
+    
+    if k*dt < 1
+    u_k = FinalV(:,k)'*sigma(Xd_k(1,k),Xd_k(2,k),t_1);
+    Ud_k(:,k)=u_k;
+    Xd_k(:,k+1) = Xd_k(:,k) + dt*f_1(Xd_k(1,k),Xd_k(2,k)) + dt*g_1*u_k;
+    else
+    u_k = FinalV(:,k)'*sigma(Xd_k(1,k),Xd_k(2,k),t_1);
+    Ud_k(:,k)=u_k;
+    Xd_k(:,k+1) = Xd_k(:,k) + dt*f_2(Xd_k(1,k),Xd_k(2,k)) + dt*g_2*u_k;
+    end
+    
+end
+
+figure()
+plot(time, Xd_k(1,:), 'r', time, Xd_k(2,:), 'k')
+xlabel('time')
+ylabel('X')
+legend('x_1','x_2')
+title('states with time transformed')
+
+%transformation to indipendent time variable to real time 
+t=zeros(1,N);
+for k = 1:N
+    if k*dt < 1
+        t(1,k)= t_1*k*dt;
+    else
+        t(1,k)=t_1 + (tf-t_1)*(k*dt-1);
+    end
+end
+
+figure()
+plot(t, Xd_k(1,:), 'r', t, Xd_k(2,:), 'k')
+xlabel('time')
+ylabel('X')
+legend('x_1','x_2')
+title('states with real time')
+
+samples = 1:N;
+
+Conv_check=zeros(N,MaxEpochNo-1);
+for k = 1:N
+    for i=1:MaxEpochNo-1
+        Conv_check(k,i) = norm (Vi_1(:,k,i+1)-Vi_1(:,k,i));
+    end
+end
+
+figure()
+plot(samples, Conv_check(:,1))
+xlabel('samples')
+ylabel('convergence')
